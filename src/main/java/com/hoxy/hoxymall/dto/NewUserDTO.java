528d03c029acae19a1f94c674bfd62bc604996ae
0Hoxy <tinyfinger@naver.com>
2024-10-18 23:07:24 +0900

    회원가입 DTO

---
+++ src/main/java/com/hoxy/hoxymall/dto/NewUserDTO.java
@@ -0,0 +1,17 @@
+package com.hoxy.hoxymall.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class NewUserDTO {
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private LocalDateTime createdAt;
+}
