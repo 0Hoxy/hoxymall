579f1756fc0241ebea83f72f502442588c0caddd
0Hoxy <tinyfinger@naver.com>
2024-10-18 23:07:09 +0900

    로그인DTO

---
+++ src/main/java/com/hoxy/hoxymall/dto/LoginUserDTO.java
@@ -0,0 +1,12 @@
+package com.hoxy.hoxymall.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginUserDTO {
+
+    private String username;
+    private String password;
+}
