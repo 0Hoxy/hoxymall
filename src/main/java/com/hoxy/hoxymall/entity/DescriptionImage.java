eef55b8389ddd2ffd7461e7cf8aedca6d930ca3d
0Hoxy <tinyfinger@naver.com>
2024-10-18 23:02:55 +0900

    사용하지 않는 생성자 삭제

--- src/main/java/com/hoxy/hoxymall/entity/DescriptionImage.java
+++ src/main/java/com/hoxy/hoxymall/entity/DescriptionImage.java
@@ -21,10 +21,6 @@ public class DescriptionImage {
     @JoinColumn(name = "product_id")
     private Product product;
 
-    // URL만 받는 생성자 추가
-    public DescriptionImage(String descriptionImgUrl) {
-        this.descriptionImgUrl = descriptionImgUrl;
-    }
 
     // Product와 URL을 받아 초기화하는 생성자
     public DescriptionImage(String descriptionImgUrl, Product product) {
