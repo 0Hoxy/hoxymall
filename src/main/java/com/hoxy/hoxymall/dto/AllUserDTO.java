13f894dc0d28be21eecab522532b3710e399c957
0Hoxy <tinyfinger@naver.com>
2024-10-18 23:05:02 +0900

    전체 회원조회를 위한 DTO

---
+++ src/main/java/com/hoxy/hoxymall/dto/AllUserDTO.java
@@ -0,0 +1,16 @@
+package com.hoxy.hoxymall.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AllUserDTO {
+
+    private String username;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    private String roles;
+
+}
